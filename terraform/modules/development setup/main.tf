ed2210b873e47d1e56ed6889516e1ba439421eb5
lentrekin1 <lancee654@gmail.com>
2023-12-21 21:36:47 -0700

    Configure Development Setup for production deployment

---
+++ terraform/modules/development setup/main.tf
@@ -0,0 +1,26 @@
+# Development Setup Terraform configuration
+terraform {
+  required_version = ">= 1.0"
+}
+
+resource "aws_development setup_cluster" "main" {
+  name     = "devmind-development setup-${var.environment}"
+  version  = var.cluster_version
+  
+  vpc_config {
+    subnet_ids = var.subnet_ids
+    security_group_ids = [aws_security_group.cluster.id]
+  }
+  
+  encryption_config {
+    resources = ["secrets"]
+    provider {
+      key_id = var.kms_key_id
+    }
+  }
+  
+  tags = merge(var.tags, {
+    Name = "devmind-development setup"
+    Type = "enterprise"
+  })
+}
